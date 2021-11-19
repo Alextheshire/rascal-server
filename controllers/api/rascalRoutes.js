3820060fae5141a4df6348d87180620208225e51
alexberes <aalexbberes@gmail.com>
2021-11-19 14:42:01 -0800

    establishes testing routes

--- controllers/api/rascalRoutes.js
+++ controllers/api/rascalRoutes.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const {User,Rascal} = require('../../models');
+const { User, Rascal } = require('../../models');
 
-module.exports=router
+
+router.get("/", (req, res) => {
+    Rascal.findAll().then(rascals => {
+        res.json(rascals)
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+})
+router.get("/id", (req, res) => {
+    Rascal.findByPk(req.params.id).then(rascal => {
+        res.json(rascal)
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+})
+router.post("/new",(req,res)=>{
+    Rascal.create(req.body)
+})
+module.exports = router
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,7 @@ const { application } = require('express');
 const express = require('express');
 const router = express.Router();
 const { User, Rascal } = require('../../models');
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
     User.findAll().then(users => res.json(users)).catch(err => {
@@ -10,7 +11,39 @@ router.get("/", (req, res) => {
     })
 })
 router.get("/:id", (req, res) => {
-    User.findAll().then(users => res.json(users)).catch(err => {
+    User.findByPk(req.params.id).then(user => res.json(user)).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+})
+router.post("/new",(req,res)=>{
+    User.create({
+        email:req.body.email,
+        password:req.body.password,
+        lastLoggedOn: Date.now()
+
+    }).then(newUser=>res.json(newUser)).catch(err=>{
+        console.log(err)
+        res.json(err)
+    })
+})
+router.post("/login",(req,res)=>{
+    User.findOne({
+        where: {
+            email:req.body.email
+        }
+
+    }).then(foundUser=>{
+        if(!foundUser){
+            res.status(401).json({ message: "incorrect email or password" })
+        }else {
+            if (bcrypt.compareSync(req.body.password, foundUser.password)){
+                res.json("Authenticated")
+            }else{
+                res.json("password bad")
+            }
+        }
+    }).catch(err=>{
         console.log(err)
         res.json(err)
     })
