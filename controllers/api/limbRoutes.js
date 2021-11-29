de2a6f5a203d855078d855f7c8877cea70ffdd0a
alexberes <aalexbberes@gmail.com>
2021-11-29 14:10:27 -0800

    added limb crud

--- controllers/api/limbRoutes.js
+++ controllers/api/limbRoutes.js
@@ -4,6 +4,11 @@ const { User, Rascal, Limb } = require('../../models');
 const tokenAuth = require("../../middleware/tokenAuth")
 const sequelize = require("../../config/connection.js")
 
+router.get('/',(req,res)=>{
+    Limb.findAll().then(data=>{
+        res.json(data)
+    })
+})
 router.get('/rascal/:id',(req,res)=>{
     Limb.findAll({RascalId:req.params.id}).then(limbs=>{
         res.json(limbs)
@@ -12,5 +17,22 @@ router.get('/rascal/:id',(req,res)=>{
         res.json(err)
     })
 })
+router.post('/post/:id',(req,res)=>{
+    Limb.create({...req.body,RascalId:req.params.id}).then(e=>{
+        res.json("success")
+    }).catch(err=>{
+        console.log(err)
+        res.json(err)
+    })
+})
+router.put('/put/:id',(req,res)=>{
+    Limb.update(req.body,{where:{RascalId:req.params.id,id:req.body.id}}).then(e=>{
+        res.json("success")
+    }).catch(err=>{
+        console.log(err)
+        res.json(err)
+    })
+})
+
 
 module.exports = router
