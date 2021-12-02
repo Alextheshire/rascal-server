3d8d827f8034e2554291ef0dffc9985d2436036a
alexberes <aalexbberes@gmail.com>
2021-12-02 11:08:20 -0800

    delete

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(routes)
 
-sequelize.sync({ force: false }).then(function() {
+sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
     });
