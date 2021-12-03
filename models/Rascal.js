a33cf7e40c9d6888c16373c7b47edc16cb4e88f1
alexberes <aalexbberes@gmail.com>
2021-12-03 14:10:20 -0800

    adding xp

--- models/Rascal.js
+++ models/Rascal.js
@@ -10,16 +10,30 @@ Rascal.init({
     },
     level: {
         type: DataTypes.INTEGER,
-        defaultValue: 0
+        defaultValue: 1
+    },
+    xp: {
+        type:DataTypes.INTEGER,
+        defaultValue:0
+    },
+    xpToLevelUp:{
+        type:DataTypes.INTEGER,
+        defaultValue:100
     },
     color:{
         type:DataTypes.STRING
     },
     happiness:{
-        type: DataTypes.DECIMAL(6,2)
+        type: DataTypes.DECIMAL(7,3)
     },
     hunger:{
-        type: DataTypes.DECIMAL(6,2)
+        type: DataTypes.DECIMAL(7,3)
+    },
+    care:{
+        type:DataTypes.DECIMAL(7,3)
+    },
+    boredom:{
+        type:DataTypes.DECIMAL(7,3)
     },
     body:{
         type:DataTypes.STRING
